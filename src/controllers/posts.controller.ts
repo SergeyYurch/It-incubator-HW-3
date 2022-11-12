1c7694067a8cec9a310fa14ca04ae2de94249797
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-12 23:08:17 +0200

    edit post controller

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -2,46 +2,59 @@ import {Router, Request, Response} from "express";
 import {validatorMiddleware} from "../middlewares/validator.middleware";
 import {validationResult} from "express-validator";
 import {postsService} from "../services/postsService";
+import {PostInputModelDto} from "./dto/postInputModel.dto";
+import {
+    RequestWithBody, RequestWithId,
+    RequestWithIdAndBody
+} from "../types/request.type";
+import {blogsService} from "../services/blogs.service";
 
 export const postsRouter = Router();
 const {validatePostInputModel} = validatorMiddleware;
 const {deletePostById, getAllPosts, editPostById, getPostById, createNewPost} = postsService;
+const {getBlogById} = blogsService;
 
 postsRouter.get('/', (req: Request, res: Response) => {
     const posts = getAllPosts();
     return res.status(200).json(posts);
 });
 
-postsRouter.post('/', validatePostInputModel(), (req: Request, res: Response) => {
+
+postsRouter.post('/', validatePostInputModel(), (req: RequestWithBody<PostInputModelDto>, res: Response) => {
     const result = validationResult(req);
-    const errors = {errorsMessages: result.array().map(e => ({message: e.msg, field: e.param}))};
-    if (!result.isEmpty()) return res.status(400).json(errors);
+    let validationError = result.array().map(e => ({message: e.msg, field: e.param}))
     const {title, shortDescription, content, blogId} = req.body;
+    const errors= (blogId && !getBlogById(blogId))
+        ? [...validationError, {message: 'Incorrect blogId',field: 'blogId'}]: [...validationError]
+    if (errors.length>0) return res.status(400).json({errorsMessages:errors});
     const createdPost = createNewPost({title, shortDescription, content, blogId});
     return createdPost ? res.status(201).json(createdPost) : res.sendStatus(500);
 });
 
-postsRouter.get('/:id', (req: Request, res: Response) => {
+postsRouter.get('/:id', (req: RequestWithId, res: Response) => {
     const id  = req.params.id;
-    if (!id) return res.sendStatus(404)
     const result = getPostById(id);
     return result ? res.status(200).json(result) : res.sendStatus(404);
 });
 
-postsRouter.put('/:id', validatePostInputModel(), (req: Request, res: Response) => {
+postsRouter.put('/:id', validatePostInputModel(), (req: RequestWithIdAndBody<PostInputModelDto>, res: Response) => {
     const id  = req.params.id;
-    if (!id ||!getPostById(id)) return res.sendStatus(404);
+    const body =req.body
     const result = validationResult(req);
     const errors = {errorsMessages: result.array().map(e => ({message: e.msg, field: e.param}))};
     if (!result.isEmpty()) return res.status(400).json(errors);
-    const resultPut = editPostById(id, req.body);
-    return resultPut ? res.sendStatus(204) : res.sendStatus(500);
+    const post: PostInputModelDto = {
+        title: body.title,
+        blogId: body.blogId,
+        content: body.content,
+        shortDescription: body.shortDescription
+    }
+    return editPostById(id, post) ? res.sendStatus(204) : res.sendStatus(404);
 });
 
-postsRouter.delete('/:id', (req: Request, res: Response) => {
+postsRouter.delete('/:id', (req: RequestWithId, res: Response) => {
     const id  = req.params.id;
-    if (!id || !getPostById(id)) return res.sendStatus(404);
-    const result = deletePostById(id);
-    return result ? res.sendStatus(204) :res.sendStatus(500);
+    if (!getPostById(id)) return res.sendStatus(404);
+    return deletePostById(id) ? res.sendStatus(204) :res.sendStatus(500);
 });
 
