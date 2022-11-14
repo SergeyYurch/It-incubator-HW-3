906fc8edd866a18ee7cc7ae5d8999f8ed79d06ed
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-14 10:15:11 +0200

    add edit

--- src/middlewares/authGuard.middleware.ts
+++ src/middlewares/authGuard.middleware.ts
@@ -10,8 +10,11 @@ const validAuthString = `Basic ${base64String}`;
 export const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
     if (noneAuthMethods.includes(req.method) || validAuthString === req.headers.authorization) {
+
         console.log('Auth is ok!');
         return next();
     }
+    console.log( noneAuthMethods.includes(req.method));
+    console.log(validAuthString === req.headers.authorization);
     res.sendStatus(401);
 };
