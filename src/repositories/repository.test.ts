83a2b934c0b597570ba3c90f2934a7fe7876e1f0
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-23 22:53:20 +0200

    comment all in repository.ts & repository.test.ts

--- src/repositories/repository.test.ts
+++ src/repositories/repository.test.ts
@@ -1,274 +1,274 @@
-import {BlogEntity} from "../services/entities/blog.entity";
-import {PostEntity} from "../services/entities/post.entity";
-import {repository, dataBase} from "./repository";
-
-
-const testBlogEntity1: BlogEntity = {
-    name: 'name1',
-    youtubeUrl: 'youtubeUrl1',
-    dateAt: '2022-11-11T07:56:46.550Z'
-
-};
-const testBlogEntity2: BlogEntity = {
-    name: 'name2',
-    youtubeUrl: 'youtubeUrl2',
-    dateAt: '2022-11-11T07:56:46.550Z'
-
-};
-const testBlogEntityUpdate: BlogEntity = {
-    name: 'name3',
-    youtubeUrl: 'youtubeUrl3',
-    dateAt: '2022-11-11T07:56:46.550Z'
-
-};
-
-const testPostEntity1: PostEntity = {
-    title: 'title1',
-    shortDescription: 'shortDescription1',
-    content: 'content 1',
-    blogId: 'blog1',
-    dateAt: '2022-11-11T07:56:46.550Z'
-};
-const testPostEntity2: PostEntity = {
-    title: 'title2',
-    shortDescription: 'shortDescription2',
-    content: 'content 2',
-    blogId: 'blog2',
-    dateAt: '2022-11-11T07:56:46.550Z'
-};
-const testPostEntityUpdate: PostEntity = {
-    title: 'title3',
-    shortDescription: 'shortDescription3',
-    content: 'content 3',
-    blogId: 'blog3',
-    dateAt: '2022-11-11T07:56:46.550Z'
-};
-const {
-    dataBaseClear,
-    returnAllBlogs,
-    createNewBlog,
-    returnBlogById,
-    updateBlogById,
-    deleteBlogById,
-    returnAllPosts,
-    createNewPost,
-    returnPostById,
-    updatePostById,
-    deletePostById
-} = repository;
-
-
-describe('repository.ts / blogs', () => {
-
-    beforeAll(() => dataBaseClear());
-
-    test('should wright new blog', () => {
-        createNewBlog(testBlogEntity1);
-        expect(dataBase.blogs[0]).toEqual({
-            id: expect.any(String),
-            name: 'name1',
-            youtubeUrl: 'youtubeUrl1',
-            dateAt: '2022-11-11T07:56:46.550Z'
-        });
-    });
-
-    test('should add blog to DB', () => {
-        createNewBlog(testBlogEntity2);
-        expect(dataBase.blogs).toEqual([
-            {
-                id: expect.any(String),
-                name: 'name1',
-                youtubeUrl: 'youtubeUrl1',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            },
-            {
-                id: expect.any(String),
-                name: 'name2',
-                youtubeUrl: 'youtubeUrl2',
-                dateAt: '2022-11-11T07:56:46.550Z'
-
-            }
-        ]);
-    });
-
-    test('should return 2 blogs from DB', () => {
-        expect(returnAllBlogs()).toEqual([
-            {
-                id: expect.any(String),
-                name: 'name1',
-                youtubeUrl: 'youtubeUrl1',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            },
-            {
-                id: expect.any(String),
-                name: 'name2',
-                youtubeUrl: 'youtubeUrl2',
-                dateAt: '2022-11-11T07:56:46.550Z'
-
-            }
-        ]);
-    });
-
-
-    test('should return 1 blog from DB', () => {
-        const id = returnAllBlogs()[1].id;
-        expect(returnBlogById(id)).toEqual(
-                {
-                    id: expect.any(String),
-                    name: 'name2',
-                    youtubeUrl: 'youtubeUrl2',
-                    dateAt: '2022-11-11T07:56:46.550Z'
-                }
-
-        );
-    });
-
-    test('should return undefined by incorrect ID from DB', () => {
-        expect(returnBlogById('any-id')).toBeUndefined();
-    });
-
-
-    test('should return 1 blog after deleting record by ID from DB & return true', () => {
-        const id = returnAllBlogs()[0].id;
-        expect(deleteBlogById(id)).toBe(true);
-        expect(dataBase.blogs).toEqual([
-            {
-                id: expect.any(String),
-                name: 'name2',
-                youtubeUrl: 'youtubeUrl2',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            }
-        ]);
-    });
-
-    test('should change blog by ID in DB and return true', () => {
-        const id = returnAllBlogs()[0].id;
-        expect(updateBlogById(id, testBlogEntityUpdate)).toBe(true);
-        expect(dataBase.blogs).toEqual([
-            {
-                id: expect.any(String),
-                name: 'name3',
-                youtubeUrl: 'youtubeUrl3',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            }
-        ]);
-    });
-
-
-});
-
-
-//  *********** Tests posts ******************
-describe('repository.ts / posts', () => {
-
-    beforeAll(() => dataBaseClear());
-
-    test('should wright new post', () => {
-        createNewPost(testPostEntity1);
-        expect(dataBase.posts[0]).toEqual({
-            id: expect.any(String),
-            title: 'title1',
-            shortDescription: 'shortDescription1',
-            content: 'content 1',
-            blogId: 'blog1',
-            dateAt: '2022-11-11T07:56:46.550Z'
-        });
-    });
-
-    test('should add post to DB', () => {
-        createNewPost(testPostEntity2);
-        expect(dataBase.posts).toEqual([
-            {
-                id: expect.any(String),
-                title: 'title1',
-                shortDescription: 'shortDescription1',
-                content: 'content 1',
-                blogId: 'blog1',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            },
-            {
-                id: expect.any(String),
-                title: 'title2',
-                shortDescription: 'shortDescription2',
-                content: 'content 2',
-                blogId: 'blog2',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            }
-        ]);
-    });
-
-    test('should return 2 posts from DB', () => {
-        expect(returnAllPosts()).toEqual([
-            {
-                id: expect.any(String),
-                title: 'title1',
-                shortDescription: 'shortDescription1',
-                content: 'content 1',
-                blogId: 'blog1',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            },
-            {
-                id: expect.any(String),
-                title: 'title2',
-                shortDescription: 'shortDescription2',
-                content: 'content 2',
-                blogId: 'blog2',
-                dateAt: '2022-11-11T07:56:46.550Z'
-
-            }
-        ]);
-    });
-
-
-    test('should return 1 post from DB', () => {
-        const id = returnAllPosts()[1].id;
-        expect(returnPostById(id)).toEqual(
-            {
-                id: expect.any(String),
-                title: 'title2',
-                shortDescription: 'shortDescription2',
-                content: 'content 2',
-                blogId: 'blog2',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            }
-
-        );
-    });
-
-    test('should return undefined by incorrect ID from DB', () => {
-        expect(returnPostById('any-id')).toBeUndefined();
-    });
-
-
-    test('should return 1 post after deleting record by ID from DB', () => {
-        const id = returnAllPosts()[0].id;
-        expect(deletePostById(id)).toBe(true);
-        expect(dataBase.posts).toEqual([
-            {
-                id: expect.any(String),
-                title: 'title2',
-                shortDescription: 'shortDescription2',
-                content: 'content 2',
-                blogId: 'blog2',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            }
-        ]);
-    });
-
-    test('should change post by ID in DB and return true', () => {
-        const id = returnAllPosts()[0].id;
-        expect(updatePostById(id, testPostEntityUpdate)).toBe(true)
-        expect(dataBase.posts).toEqual([
-            {
-                id: expect.any(String),
-                title: 'title3',
-                shortDescription: 'shortDescription3',
-                content: 'content 3',
-                blogId: 'blog3',
-                dateAt: '2022-11-11T07:56:46.550Z'
-            }
-        ]);
-    });
-
-
-});
+// import {BlogEntity} from "../services/entities/blog.entity";
+// import {PostEntity} from "../services/entities/post.entity";
+// import {repository, dataBase} from "./repository";
+//
+//
+// const testBlogEntity1: BlogEntity = {
+//     name: 'name1',
+//     websiteUrl: 'websiteUrl1',
+//     createdAt: '2022-11-11T07:56:46.550Z'
+//
+// };
+// const testBlogEntity2: BlogEntity = {
+//     name: 'name2',
+//     websiteUrl: 'websiteUrl2',
+//     createdAt: '2022-11-11T07:56:46.550Z'
+//
+// };
+// const testBlogEntityUpdate: BlogEntity = {
+//     name: 'name3',
+//     websiteUrl: 'websiteUrl3',
+//     createdAt: '2022-11-11T07:56:46.550Z'
+//
+// };
+//
+// const testPostEntity1: PostEntity = {
+//     title: 'title1',
+//     shortDescription: 'shortDescription1',
+//     content: 'content 1',
+//     blogId: 'blog1',
+//     createdAt: '2022-11-11T07:56:46.550Z'
+// };
+// const testPostEntity2: PostEntity = {
+//     title: 'title2',
+//     shortDescription: 'shortDescription2',
+//     content: 'content 2',
+//     blogId: 'blog2',
+//     createdAt: '2022-11-11T07:56:46.550Z'
+// };
+// const testPostEntityUpdate: PostEntity = {
+//     title: 'title3',
+//     shortDescription: 'shortDescription3',
+//     content: 'content 3',
+//     blogId: 'blog3',
+//     createdAt: '2022-11-11T07:56:46.550Z'
+// };
+// const {
+//     dataBaseClear,
+//     getAllBlogs,
+//     createNewBlog,
+//     getBlogById,
+//     updateBlogById,
+//     deleteBlogById,
+//     getAllPosts,
+//     createNewPost,
+//     getPostById,
+//     updatePostById,
+//     deletePostById
+// } = repository;
+//
+//
+// describe('repository.ts / blogs', () => {
+//
+//     beforeAll(() => dataBaseClear());
+//
+//     test('should wright new blog', () => {
+//         createNewBlog(testBlogEntity1);
+//         expect(dataBase.blogs[0]).toEqual({
+//             id: expect.any(String),
+//             name: 'name1',
+//             websiteUrl: 'websiteUrl1',
+//             createdAt: '2022-11-11T07:56:46.550Z'
+//         });
+//     });
+//
+//     test('should add blog to DB', () => {
+//         createNewBlog(testBlogEntity2);
+//         expect(dataBase.blogs).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 name: 'name1',
+//                 websiteUrl: 'websiteUrl1',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             },
+//             {
+//                 id: expect.any(String),
+//                 name: 'name2',
+//                 websiteUrl: 'websiteUrl2',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//
+//             }
+//         ]);
+//     });
+//
+//     test('should return 2 blogs from DB', () => {
+//         expect(getAllBlogs()).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 name: 'name1',
+//                 websiteUrl: 'websiteUrl1',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             },
+//             {
+//                 id: expect.any(String),
+//                 name: 'name2',
+//                 websiteUrl: 'websiteUrl2',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//
+//             }
+//         ]);
+//     });
+//
+//
+//     test('should return 1 blog from DB', () => {
+//         const id = getAllBlogs()[1].id;
+//         expect(getBlogById(id)).toEqual(
+//                 {
+//                     id: expect.any(String),
+//                     name: 'name2',
+//                     websiteUrl: 'websiteUrl2',
+//                     createdAt: '2022-11-11T07:56:46.550Z'
+//                 }
+//
+//         );
+//     });
+//
+//     test('should return undefined by incorrect ID from DB', () => {
+//         expect(getBlogById('any-id')).toBeUndefined();
+//     });
+//
+//
+//     test('should return 1 blog after deleting record by ID from DB & return true', () => {
+//         const id = getAllBlogs()[0].id;
+//         expect(deleteBlogById(id)).toBe(true);
+//         expect(dataBase.blogs).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 name: 'name2',
+//                 websiteUrl: 'websiteUrl2',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             }
+//         ]);
+//     });
+//
+//     test('should change blog by ID in DB and return true', () => {
+//         const id = getAllBlogs()[0].id;
+//         expect(updateBlogById(id, testBlogEntityUpdate)).toBe(true);
+//         expect(dataBase.blogs).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 name: 'name3',
+//                 websiteUrl: 'websiteUrl3',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             }
+//         ]);
+//     });
+//
+//
+// });
+//
+//
+// //  *********** Tests posts ******************
+// describe('repository.ts / posts', () => {
+//
+//     beforeAll(() => dataBaseClear());
+//
+//     test('should wright new post', () => {
+//         createNewPost(testPostEntity1);
+//         expect(dataBase.posts[0]).toEqual({
+//             id: expect.any(String),
+//             title: 'title1',
+//             shortDescription: 'shortDescription1',
+//             content: 'content 1',
+//             blogId: 'blog1',
+//             createdAt: '2022-11-11T07:56:46.550Z'
+//         });
+//     });
+//
+//     test('should add post to DB', () => {
+//         createNewPost(testPostEntity2);
+//         expect(dataBase.posts).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 title: 'title1',
+//                 shortDescription: 'shortDescription1',
+//                 content: 'content 1',
+//                 blogId: 'blog1',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             },
+//             {
+//                 id: expect.any(String),
+//                 title: 'title2',
+//                 shortDescription: 'shortDescription2',
+//                 content: 'content 2',
+//                 blogId: 'blog2',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             }
+//         ]);
+//     });
+//
+//     test('should return 2 posts from DB', () => {
+//         expect(getAllPosts()).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 title: 'title1',
+//                 shortDescription: 'shortDescription1',
+//                 content: 'content 1',
+//                 blogId: 'blog1',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             },
+//             {
+//                 id: expect.any(String),
+//                 title: 'title2',
+//                 shortDescription: 'shortDescription2',
+//                 content: 'content 2',
+//                 blogId: 'blog2',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//
+//             }
+//         ]);
+//     });
+//
+//
+//     test('should return 1 post from DB', () => {
+//         const id = getAllPosts()[1].id;
+//         expect(getPostById(id)).toEqual(
+//             {
+//                 id: expect.any(String),
+//                 title: 'title2',
+//                 shortDescription: 'shortDescription2',
+//                 content: 'content 2',
+//                 blogId: 'blog2',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             }
+//
+//         );
+//     });
+//
+//     test('should return undefined by incorrect ID from DB', () => {
+//         expect(getPostById('any-id')).toBeUndefined();
+//     });
+//
+//
+//     test('should return 1 post after deleting record by ID from DB', () => {
+//         const id = getAllPosts()[0].id;
+//         expect(deletePostById(id)).toBe(true);
+//         expect(dataBase.posts).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 title: 'title2',
+//                 shortDescription: 'shortDescription2',
+//                 content: 'content 2',
+//                 blogId: 'blog2',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             }
+//         ]);
+//     });
+//
+//     test('should change post by ID in DB and return true', () => {
+//         const id = getAllPosts()[0].id;
+//         expect(updatePostById(id, testPostEntityUpdate)).toBe(true)
+//         expect(dataBase.posts).toEqual([
+//             {
+//                 id: expect.any(String),
+//                 title: 'title3',
+//                 shortDescription: 'shortDescription3',
+//                 content: 'content 3',
+//                 blogId: 'blog3',
+//                 createdAt: '2022-11-11T07:56:46.550Z'
+//             }
+//         ]);
+//     });
+//
+//
+// });
--- src/repositories/repository.ts
+++ src/repositories/repository.ts
@@ -1,102 +1,102 @@
-import {v4 as uuidv4} from 'uuid';
-import {PostEntity} from "../services/entities/post.entity";
-import {BlogEntity} from "../services/entities/blog.entity";
-import {
-    BlogDbInterface,
-    DataBaseType,
-    PostDbInterface,
-    RepositoryInterface
-} from "./repository.interface";
-
-export const dataBase: DataBaseType = {
-    posts: [
-        {
-            id: '1',
-            title: 'Post title 1',
-            shortDescription: 'shortDescription 1',
-            content: 'content',
-            blogId: '1',
-            dateAt: '2022-11-12T12:10:56.185Z'
-        }
-    ],
-    blogs: [
-        {
-            id: '1',
-            name: 'Blog name1',
-            youtubeUrl: 'https://www.youtube.com/watch?v=BMo29Gkx8K0&list=PLbLBXDhswD1eq9M-KavTK98Z4OI00VbsW',
-            dateAt: '2022-11-12T12:10:56.185Z'
-        },
-        {
-            id: '2',
-            name: 'Blog name2',
-            youtubeUrl: 'https://www.youtube.com/watch?v=BMo29Gkx8K0&list=PLbLBXDhswD1eq9M-KavTK98Z4OI00VbsW',
-            dateAt: '2022-11-12T12:10:56.185Z'
-        }
-    ]
-};
-
-export const repository: RepositoryInterface =
-    {
-        dataBaseClear: (): boolean => {
-            dataBase.posts = [];
-            dataBase.blogs = [];
-            return true;
-        },
-
-        returnAllBlogs: (): BlogDbInterface[] => {
-            return dataBase.blogs;
-        },
-
-        createNewBlog: (inputBlog: BlogEntity): BlogDbInterface  => {
-            const id: string = uuidv4();
-            const newBlog: BlogDbInterface = {
-                id,
-                name: inputBlog.name,
-                youtubeUrl: inputBlog.youtubeUrl,
-                dateAt: inputBlog.dateAt
-            };
-            dataBase.blogs.push(newBlog);
-            return newBlog;
-        },
-
-        returnBlogById: (id: string): BlogDbInterface | undefined => {
-            return dataBase.blogs.find(b => b.id === id);
-        },
-
-        updateBlogById: (id: string, inputBlog: BlogEntity): boolean => {
-            const newBlog: BlogDbInterface = {id, ...inputBlog};
-            dataBase.blogs = dataBase.blogs.map(b => b.id === id ? newBlog : b);
-            return true;
-        },
-
-        deleteBlogById: (id: string): boolean => {
-            dataBase.blogs = dataBase.blogs.filter(b => b.id !== id);
-            return true;
-        },
-
-        returnAllPosts: (): PostDbInterface[] => {
-            return dataBase.posts;
-        },
-
-        createNewPost: (inputPost: PostEntity): PostDbInterface  => {
-            const id: string = uuidv4();
-            const newPost: PostDbInterface = {id, ...inputPost};
-            dataBase.posts.push(newPost);
-            return newPost;
-        },
-
-        returnPostById: (id: string): PostDbInterface | undefined => {
-            return dataBase.posts.find(p => p.id === id);
-        },
-
-        updatePostById: (id: string, inputPost: PostEntity): boolean => {
-            const newPost: PostDbInterface = {id, ...inputPost};
-            dataBase.posts = dataBase.posts.map(p => p.id === id ? newPost : p);
-            return true;
-        },
-
-        deletePostById: (id: string): boolean => {
-            dataBase.posts = dataBase.posts.filter(p => p.id !== id);
-            return true;
-        }
-    };
+// import {v4 as uuidv4} from 'uuid';
+// import {PostEntity} from "../services/entities/post.entity";
+// import {BlogEntity} from "../services/entities/blog.entity";
+// import {
+//     BlogDbInterface,
+//     DataBaseType,
+//     PostDbInterface,
+//     RepositoryInterface
+// } from "./repository.interface";
+//
+// export const dataBase: DataBaseType = {
+//     posts: [
+//         {
+//             id: '1',
+//             title: 'Post title 1',
+//             shortDescription: 'shortDescription 1',
+//             content: 'content',
+//             blogId: '1',
+//             createdAt: '2022-11-12T12:10:56.185Z'
+//         }
+//     ],
+//     blogs: [
+//         {
+//             id: '1',
+//             name: 'Blog name1',
+//             websiteUrl: 'https://www.youtube.com/watch?v=BMo29Gkx8K0&list=PLbLBXDhswD1eq9M-KavTK98Z4OI00VbsW',
+//             createdAt: '2022-11-12T12:10:56.185Z'
+//         },
+//         {
+//             id: '2',
+//             name: 'Blog name2',
+//             websiteUrl: 'https://www.youtube.com/watch?v=BMo29Gkx8K0&list=PLbLBXDhswD1eq9M-KavTK98Z4OI00VbsW',
+//             createdAt: '2022-11-12T12:10:56.185Z'
+//         }
+//     ]
+// };
+//
+// export const repository: RepositoryInterface =
+//     {
+//         dataBaseClear: (): boolean => {
+//             dataBase.posts = [];
+//             dataBase.blogs = [];
+//             return true;
+//         },
+//
+//         getAllBlogs: (): BlogDbInterface[] => {
+//             return dataBase.blogs;
+//         },
+//
+//         createNewBlog: (inputBlog: BlogEntity): BlogDbInterface  => {
+//             const id: string = uuidv4();
+//             const newBlog: BlogDbInterface = {
+//                 id,
+//                 name: inputBlog.name,
+//                 websiteUrl: inputBlog.websiteUrl,
+//                 createdAt: inputBlog.dateAt
+//             };
+//             dataBase.blogs.push(newBlog);
+//             return newBlog;
+//         },
+//
+//         getBlogById: (id: string): BlogDbInterface | undefined => {
+//             return dataBase.blogs.find(b => b.id === id);
+//         },
+//
+//         updateBlogById: (id: string, inputBlog: BlogEntity): boolean => {
+//             const newBlog: BlogDbInterface = {id, ...inputBlog};
+//             dataBase.blogs = dataBase.blogs.map(b => b.id === id ? newBlog : b);
+//             return true;
+//         },
+//
+//         deleteBlogById: (id: string): boolean => {
+//             dataBase.blogs = dataBase.blogs.filter(b => b.id !== id);
+//             return true;
+//         },
+//
+//         getAllPosts: (): PostDbInterface[] => {
+//             return dataBase.posts;
+//         },
+//
+//         createNewPost: (inputPost: PostEntity): PostDbInterface  => {
+//             const id: string = uuidv4();
+//             const newPost: PostDbInterface = {id, ...inputPost};
+//             dataBase.posts.push(newPost);
+//             return newPost;
+//         },
+//
+//         getPostById: (id: string): PostDbInterface | undefined => {
+//             return dataBase.posts.find(p => p.id === id);
+//         },
+//
+//         updatePostById: (id: string, inputPost: PostEntity): boolean => {
+//             const newPost: PostDbInterface = {id, ...inputPost};
+//             dataBase.posts = dataBase.posts.map(p => p.id === id ? newPost : p);
+//             return true;
+//         },
+//
+//         deletePostById: (id: string): boolean => {
+//             dataBase.posts = dataBase.posts.filter(p => p.id !== id);
+//             return true;
+//         }
+//     };
