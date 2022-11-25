16f69b61c7a38396b94b4e915ebedb9a4e4398ce
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-25 19:03:17 +0200

    refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
 import {testingRouter} from "./controllers/testing.controller";
 import {blogsRouter} from "./controllers/blogs.controller";
 import {postsRouter} from "./controllers/posts.controller";
-// import {authGuardMiddleware} from "./middlewares/authGuard.middleware";
-
-dotenv.config();
 
 export const app = express();
 app.use(express.json())
-// app.use(authGuardMiddleware);
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 app.use('/testing', testingRouter)
+
