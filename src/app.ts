17120adbb3e0294377a0d0132796aee88420075f
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-25 18:44:01 +0200

    refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,15 @@
-import express, {Express} from 'express';
+import express from 'express';
 import * as dotenv from 'dotenv';
 import {testingRouter} from "./controllers/testing.controller";
 import {blogsRouter} from "./controllers/blogs.controller";
 import {postsRouter} from "./controllers/posts.controller";
 // import {authGuardMiddleware} from "./middlewares/authGuard.middleware";
-import {Server} from "http";
-import {runDB} from "./repositories/db";
 
 dotenv.config();
 
-export class App {
-    app: Express;
-    port: number;
-    server!: Server;
-
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT ? +process.env.PORT : 5001;
-    }
-
-    useMiddleware(): void {
-        this.app.use(express.json());
-        // this.app.use(authGuardMiddleware);
-    }
-
-    useRouters(): void {
-        this.app.use('/blogs', blogsRouter);
-        this.app.use('/posts', postsRouter);
-        this.app.use('/testing', testingRouter);
-    }
-
-    public async init() {
-        this.useMiddleware();
-        this.useRouters();
-        await runDB();
-        this.server = this.app.listen(this.port, ()=>{
-            console.log(`[App] Server on post:${this.port} is start `);
-        });
-
-    }
-}
+export const app = express();
+app.use(express.json())
+// app.use(authGuardMiddleware);
+app.use('/blogs', blogsRouter)
+app.use('/posts', postsRouter)
+app.use('/testing', testingRouter)
