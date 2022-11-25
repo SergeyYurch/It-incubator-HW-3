c25c024652d20c900f31445df419f2fc5b51c917
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-25 19:36:53 +0200

    refactor db.ts

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -4,6 +4,7 @@ import {PostEntity} from "../services/entities/post.entity";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+
 const mongoUri = process.env.MONGO_URI
 if (!mongoUri){
     throw new Error('!!!Mongo URI does not found')
