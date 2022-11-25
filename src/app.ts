13b13952acfb6b9e1e3c528853541a35134cb4cd
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-25 19:54:15 +0200

    refactor add authMiddleware

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import {testingRouter} from "./controllers/testing.controller";
 import {blogsRouter} from "./controllers/blogs.controller";
 import {postsRouter} from "./controllers/posts.controller";
+import {authGuardMiddleware} from "./middlewares/authGuard.middleware";
 
 export const app = express();
 app.use(express.json())
+app.use(authGuardMiddleware);
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 app.use('/testing', testingRouter)
